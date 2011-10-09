d1827558a8356bfb5bdb005a55da616d39e35269
Albert Peng <albertpeng.ap@gmail.com>
2011-10-08 18:23:19 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spellcheck::Application.config.secret_token = '13b32bac99e2adb80a2df8c496c26283ee00ee859ed77600cdc5918de786ad45a407650947ed86a613e2403b59d551181f7f2f4bfa9414544b5419fc83d9f0f5'
